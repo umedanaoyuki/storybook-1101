b34914af09c59fdd85cad377c3a58cfc7f7fb84b
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2024-10-29 23:40:06 +0900

    feat: SSRでもstyled-componentsが使用できるようにする

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  compiler: {
+    // styled-componentsの有効化
+    styledComponents: true,
+  },
 };
 
 export default nextConfig;
