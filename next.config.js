7bff731c542326bd69a5bfbb35cdb084714ceba9
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2024-11-02 22:41:28 +0900

    feat: next.jsバージョンを14へ下げる

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // ここに設定を書く
+  reactStrictMode: true,
+  // その他の設定...
+  compiler: {
+    // styledComponents有効化
+    styledComponents: true,
+  },
+};
+
+module.exports = nextConfig;
