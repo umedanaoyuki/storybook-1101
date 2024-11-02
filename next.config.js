4303ad9fbfbf1d9c766cf11cf162e482b09bd2e8
NAOYUKI UMEDA <naoyuki_3075@yahoo.co.jp>
2024-11-03 02:13:12 +0900

    json serverの設定/テスト環境設定

--- next.config.js
+++ next.config.js
@@ -1,12 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // ここに設定を書く
+  // Enable React strict mode
   reactStrictMode: true,
-  // その他の設定...
-  compiler: {
-    // styledComponents有効化
-    styledComponents: true,
-  },
+
+  // Other settings...
+  compiler: (() => {
+    let compilerConfig = {
+      // Enable styled-components
+      styledComponents: true,
+    };
+
+    if (process.env.NODE_ENV === 'production') {
+      // Remove data-testid attribute in production for React Testing Library
+      compilerConfig = {
+        ...compilerConfig,
+        reactRemoveProperties: { properties: ['^data-testid$'] },
+      };
+    }
+
+    return compilerConfig;
+  })(),
 };
 
 module.exports = nextConfig;
